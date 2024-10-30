52c3457e1bde663584eaf1cb274e72befda868b1
Ciraqui <gciraqui.iftm@gmail.com>
2024-10-30 12:52:47 -0300

    CORS removed

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocs = require('./configs/swaggerConfig')
 const env = require('dotenv').config();
-const cors = require('cors');
 const app = express();
-
-app.use(cors({
-  origin: 'http://localhost:5173'
-}));
-
 app.use(express.json());
 
 const usuarioRoutes = require("./src/routes/usuario")
